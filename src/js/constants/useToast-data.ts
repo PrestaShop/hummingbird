8063dc8cabfc6bb309679cf590b3c72b9c8aef2d
Mahmoud Parvazi <85633460+mparvazi@users.noreply.github.com>
2022-03-09 17:32:46 +0330

    Update useToast-data.ts

--- src/js/constants/useToast-data.ts
+++ src/js/constants/useToast-data.ts
@@ -23,6 +23,8 @@
  * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
  */
 
+import {Toast} from 'bootstrap';
+
 export const Theme = {
   light: 'bg-light text-dark border-1',
   dark: 'bg-dark text-light',
@@ -62,9 +64,12 @@ export interface Options {
 }
 
 export interface Instance {
-  show: () => void;
-  hide: () => void;
-  remove: () => void;
-  element: () => HTMLElement | void;
-  content: (markup?: string) => string | void;
+  instance: Toast | null;
+  element: HTMLElement | null;
+  content: HTMLElement | null;
+  show: () => boolean;
+  hide: () => boolean;
+  dispose: () => boolean;
+  remove: () => boolean;
+  message: (markup?: string) => string | boolean;
 }

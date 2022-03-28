bf710c819a8e03cb06d6d2325a982338918e1cd1
Mahmoud Parvazi <85633460+mparvazi@users.noreply.github.com>
2022-03-28 14:24:17 +0430

    Update useQuantityInput-data.ts

--- src/js/constants/mocks/useQuantityInput-data.ts
+++ src/js/constants/mocks/useQuantityInput-data.ts
@@ -23,7 +23,7 @@
  * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
  */
 
-export const delay = 25;
+export const delay = 5;
 export const ProductId = 1;
 
 export const ProductLineTemplate = `

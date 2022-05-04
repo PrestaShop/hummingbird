ec3e2e40f47f7db0623a2c32a58c3420588b87b9
NeOMakinG <valentin.szczupak@prestashop.com>
2022-05-04 14:27:20 +0200

    Improve template
    Co-authored-by: Mahmoud Parvazi <85633460+mparvazi@users.noreply.github.com>

--- src/js/constants/mocks/useProgressRing-data.ts
+++ src/js/constants/mocks/useProgressRing-data.ts
@@ -23,24 +23,63 @@
  * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
  */
 
-/* eslint-disable max-len */
-
 export const Template = `
   <svg class="progress-ring text-success col-4" width="74" height="74" style="width: 74px; height: 74px;">
-    <circle class="progress-ring__background-circle" stroke-width="4" fill="transparent" r="29" cx="37" cy="37"></circle>
-    <circle class="progress-ring__circle" stroke="currentColor" stroke-width="4" data-percent="75" fill="transparent" r="29" cx="37" cy="37" style="stroke-dasharray: 182.212, 182.212; stroke-dashoffset: 45.5531;"></circle>
-    <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">3 / 4</text>
+    <circle
+      class="progress-ring__background-circle" 
+      stroke-width="4" 
+      fill="transparent"
+      r="29"
+      cx="37" 
+      cy="37"
+    ></circle>
+    <circle 
+      class="progress-ring__circle" 
+      stroke="currentColor" 
+      stroke-width="4" 
+      data-percent="0" 
+      fill="transparent" 
+      r="29" 
+      cx="37" 
+      cy="37"
+      style="stroke-dasharray: 182.212, 182.212; stroke-dashoffset: 0;"
+    ></circle>
+    <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle"></text>
   </svg>
  `;
 
 export const TemplateWithoutText = `
   <svg class="progress-ring text-success col-4" width="74" height="74" style="width: 74px; height: 74px;">
-    <circle class="progress-ring__background-circle" stroke-width="4" fill="transparent" r="29" cx="37" cy="37"></circle>
-    <circle class="progress-ring__circle" stroke="currentColor" stroke-width="4" data-percent="75" fill="transparent" r="29" cx="37" cy="37" style="stroke-dasharray: 182.212, 182.212; stroke-dashoffset: 45.5531;"></circle>
+    <circle
+      class="progress-ring__background-circle" 
+      stroke-width="4" 
+      fill="transparent"
+      r="29"
+      cx="37" 
+      cy="37"
+    ></circle>
+    <circle 
+      class="progress-ring__circle" 
+      stroke="currentColor" 
+      stroke-width="4" 
+      data-percent="0" 
+      fill="transparent" 
+      r="29" 
+      cx="37" 
+      cy="37"
+      style="stroke-dasharray: 182.212, 182.212; stroke-dashoffset: 0;"
+    ></circle>
   </svg>
  `;
+
+export const TemplateWithoutCircle = `
+ <svg class="progress-ring text-success col-4" width="74" height="74" style="width: 74px; height: 74px;">
+   <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle"></text>
+ </svg>
+`;
 
 export default {
   Template,
   TemplateWithoutText,
 };
+

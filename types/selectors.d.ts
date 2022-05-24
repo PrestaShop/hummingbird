794851774723efabe61306998a7e930b9e8290ca
NeOMakinG <valentin.szczupak@prestashop.com>
2022-05-24 13:22:30 +0200

    Type every selectors

--- types/selectors.d.ts
+++ types/selectors.d.ts
@@ -4,134 +4,134 @@
  */
 
 declare type facetedsearch = {
-  range: '.js-faceted-slider',
-  rangeContainer: '.js-faceted-slider-container',
-  filterSlider: '.js-faceted-filter-slider',
+  range: string,
+  rangeContainer: string,
+  filterSlider: string,
 };
 
-declare type pageLoader = '.js-page-loader';
+declare type pageLoader = string;
 
 declare type listing = {
-  searchFilterToggler: '#search_filter_toggler, .js-search-toggler',
-  searchFiltersWrapper: '#search_filters_wrapper',
-  searchFilterControls: '#search_filter_controls',
-  searchFilters: '#search-filters',
-  activeSearchFilters: '#js-active-search-filters',
-  listTop: '#js-product-list-top',
-  product: '.js-product',
-  list: '#js-product-list',
-  listBottom: '#js-product-list-bottom',
-  listHeader: '#js-product-list-header',
-  searchFiltersClearAll: '.js-search-filters-clear-all',
-  searchLink: '.js-search-link',
+  searchFilterToggler: string,
+  searchFiltersWrapper: string,
+  searchFilterControls: string,
+  searchFilters: string,
+  activeSearchFilters: string,
+  listTop: string,
+  product: string,
+  list: string,
+  listBottom: string,
+  listHeader: string,
+  searchFiltersClearAll: string,
+  searchLink: string,
 };
 
 declare type cart = {
-  discountCode: '.js-discount .js-code',
-  discountName: '[name=discount_name]',
-  displayPromo: '.display-promo',
-  promoCode: '#promo-code',
+  discountCode: string,
+  discountName: string,
+  displayPromo: string,
+  promoCode: string,
 };
 
 declare type blockcart = {
-  modal: '#blockcart-modal',
+  modal: string,
 };
 
 declare type currencySelector = {
-  currencySelector: '.js-currency-selector',
+  currencySelector: string,
 };
 
 declare type languageSelector = {
-  languageSelector: '.js-language-selector',
+  languageSelector: string,
 };
 
 declare type searchBar = {
-  searchCanvas: '.js-search-offcanvas',
-  searchWidget: '.js-search-widget',
-  searchDropdown: '.js-search-dropdown',
-  searchResults: '.js-search-results',
-  searchTemplate: '.js-search-template',
-  searchInput: '.js-search-input',
+  searchCanvas: string,
+  searchWidget: string,
+  searchDropdown: string,
+  searchResults: string,
+  searchTemplate: string,
+  searchInput: string,
 };
 
 declare type checkout = {
   steps: {
-    item: '.js-step-item',
-    current: '.js-current-step',
-    shownResponsiveStep: '.checkout__steps__step:not(.d-none)',
+    item: string,
+    current: string,
+    shownResponsiveStep: string,
     specificStep: (param: string | undefined) => string,
     specificStepContent: (param: string | undefined) => string,
     backButton: (param: string | undefined) => string,
   },
-  actionsButtons: '.js-back, .js-edit-addresses, .js-edit-shipping',
-  termsLink: '.js-terms a',
-  checkoutModal: '#checkout-modal',
+  actionsButtons: string,
+  termsLink: string,
+  checkoutModal: string,
 };
 
 declare type progressRing = {
   checkout: {
-    element: '.progress-ring',
-    circle: '.progress-ring__circle',
-    backgroundCircle: '.progress-ring__background-circle',
+    element: string,
+    circle: string,
+    backgroundCircle: string,
   },
-  text: '.progress-ring text',
+  text: string,
 };
 
 declare type mobileMenu = {
-  openChildsButton: '.js-menu-open-child',
-  backTitle: '.js-menu-back-title',
-  backButton: '.js-back-button',
-  menuCanvas: '.js-menu-canvas',
-  menuCurrent: '.menu--current',
+  openChildsButton: string,
+  backTitle: string,
+  backButton: string,
+  menuCanvas: string,
+  menuCurrent: string,
   specificParent: (param: string | number | undefined) => string,
   specificChild: (param: string | number | undefined) => string,
 };
 
 declare type visiblePassword = {
-  visiblePassword: '.js-visible-password',
+  visiblePassword: string,
 };
 
 declare type desktopMenu = {
-  dropdownToggles: '.js-menu-desktop .dropdown .dropdown-toggle[data-depth]',
+  dropdownToggles: string,
   dropdownItemAnchor: (depth: number) => string,
 };
 
 declare type quantityInput = {
-  default: '.js-quantity-button',
-  modal: '.modal-dialog .js-quantity-button',
-  increment: '.js-increment-button',
-  decrement: '.js-decrement-button',
-  confirm: '.confirmation',
-  icon: '.material-icons',
-  spinner: '.spinner-border',
+  default: string,
+  modal: string,
+  increment: string,
+  decrement: string,
+  confirm: string,
+  icon: string,
+  spinner: string,
   alert: (param: string) => string,
 };
 
 type SelectorsMap = {
   alert: {
-    selector: '#notifications .container',
-    alert: '.alert',
-    heading: '.alert-heading',
-    body: '.alert-body',
-    icon: '.material-icons',
-    close: '.btn-close',
+    selector: string,
+    alert: string,
+    heading: string,
+    body: string,
+    icon: string,
+    close: string,
   },
   toast: {
-    container: '#js-toast-container',
-    template: '.js-toast-template',
-    toast: '.toast',
-    body: '.toast-body',
-    close: '.btn-close',
+    container: string,
+    template: string,
+    toast: string,
+    body: string,
+    close: string,
   },
   product: {
-    carousel: '.js-product-carousel',
-    miniature: '.js-product-miniature',
-    thumbnail: '.js-thumb-container',
+    carousel: string,
+    miniature: string,
+    thumbnail: string,
     activeThumbail: (id: number) => string,
   },
-  modalBody: '.modal-body',
-  pageCms: '.page-cms',
-  quickview: '.js-quickview',
+  modalBody: string,
+  pageCms: string,
+  quickview: string,
   facetedsearch: facetedsearch,
   pageLoader: pageLoader,
   listing: listing,

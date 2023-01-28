74b9ab25bd0a30092d644eea8ee1f751f39222c0
nizelg <104589134+nizelg@users.noreply.github.com>
2023-01-28 16:05:29 +0300

    Update theme.ts

--- src/js/theme.ts
+++ src/js/theme.ts
@@ -34,7 +34,7 @@ $(() => {
 
   // @TODO: Fix this on core.js side inside a major version of PrestaShop instead of minor
   // For reference: https://github.com/PrestaShop/hummingbird/pull/418#discussion_r1061938669
-  document.querySelectorAll<HTMLInputElement>('[name="token"]').forEach((el) => {
+  document.querySelectorAll<HTMLInputElement>('#add_to_card_hp [name="token"]').forEach((el) => {
     el.value = prestashop.static_token;
   });
 

f93b26a1b29b17bf3dadda9d1682ccc3a9341f2b
Yannick Armand <yannick.armand@protonmail.com>
2023-06-13 22:50:29 +0200

    Show/Hide Password: Accessibility on the button

--- src/js/visible-password.ts
+++ src/js/visible-password.ts
@@ -9,16 +9,24 @@ const initVisiblePassword = () => {
   const visiblePasswordList = document.querySelectorAll(visiblePasswordMap.visiblePassword);
 
   visiblePasswordList.forEach((input: HTMLInputElement) => {
-    const button = input?.nextElementSibling;
+    const button = input?.nextElementSibling as HTMLElement;
 
     button?.addEventListener('click', () => {
       const newType = input.getAttribute('type') === 'text' ? 'password' : 'text';
       input.setAttribute('type', newType);
+      button.setAttribute('aria-expanded', newType === 'text' ? 'true' : 'false');
 
       const icon = button.firstElementChild;
 
       if (icon) {
         icon.innerHTML = newType === 'text' ? 'visibility_off' : 'visibility';
+
+        const textShow = button.dataset.textShow;
+        const textHide = button.dataset.textHide;
+
+        if (textShow && textHide) {
+          button.setAttribute('aria-label', newType === 'text' ? textHide : textShow);
+        }
       }
     });
   });

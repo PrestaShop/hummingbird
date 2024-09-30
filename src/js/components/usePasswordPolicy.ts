5f0be62e7249e67027f70dd08afbb93531b7815e
Matthias Goudjil <matthias.goudjil@prestashop.com>
2024-09-30 12:01:09 +0200

    chore: update readme (node 20 required), userPasswordPolicy.ts :delete console.log(error)

--- src/js/components/usePasswordPolicy.ts
+++ src/js/components/usePasswordPolicy.ts
@@ -164,8 +164,7 @@ const usePasswordPolicy = (selector: string): PasswordPolicyReturn => {
         const hints = ((content) => {
           try {
             return JSON.parse(content);
-          } catch (error) {
-            console.error(error);
+          } catch {
             return false;
           }
         })(hintElement.innerHTML);

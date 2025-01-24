9ed6f33939b393946e809128696086e29dd247c4
tblivet <thibault.blivet@prestashop.com>
2025-01-24 14:42:24 +0100

    feat: add prettier on SCSS

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  overrides: [
+    {
+      files: "*.scss",
+      options: {
+        printWidth: 100,
+        tabWidth: 2,
+        useTabs: false,
+        singleQuote: false,
+        bracketSpacing: true,
+        parser: "scss",
+      },
+    },
+  ],
+};

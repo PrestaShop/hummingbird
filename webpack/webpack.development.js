f85c8ee04230d9d1df6992dc162cff0a32b68e07
tblivet <thibault.blivet@prestashop.com>
2025-10-13 15:19:12 +0200

    chore: config cleanup + update workflows

--- webpack/webpack.development.js
+++ webpack/webpack.development.js
@@ -2,16 +2,25 @@ const { configureDevServer } = require('./webpack.parts');
 const { HotAcceptPlugin } = require('hot-accept-webpack-plugin');
 const webpack = require('webpack');
 
-exports.developmentConfig = ({ port, publicPath, serverAddress, siteURL, entriesArray }) => ({
-  devtool: "cheap-source-map",
-  devServer: configureDevServer(serverAddress, publicPath, port, siteURL),
-  watchOptions: {
-    ignored: /node_modules/,
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HotAcceptPlugin({
-      test: Object.keys(entriesArray).map(el => `${el}.js`)
-    })
-  ]
-});
+exports.developmentConfig = ({ port, publicPath, serverAddress, siteURL, entriesArray, isDevServer = false }) => {
+  const plugins = [];
+  
+  // Only enable HMR when using webpack serve (dev server)
+  if (isDevServer) {
+    plugins.push(
+      new webpack.HotModuleReplacementPlugin(),
+      new HotAcceptPlugin({
+        test: Object.keys(entriesArray).map(el => `${el}.js`)
+      })
+    );
+  }
+
+  return {
+    devtool: "cheap-source-map",
+    devServer: isDevServer ? configureDevServer(serverAddress, publicPath, port, siteURL) : undefined,
+    watchOptions: {
+      ignored: /node_modules/,
+    },
+    plugins
+  };
+};

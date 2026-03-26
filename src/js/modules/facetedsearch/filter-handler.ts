58822cca0325a471a454049d14c69eb86994f6c6
Alexis Guyomar <alexis.guyomar@prestashop.com>
2026-03-26 15:33:05 +0100

    refactor: use native URL and URLSearchParams APIs for query parameter handling instead of custom parsing logic.

--- src/js/modules/facetedsearch/filter-handler.ts
+++ src/js/modules/facetedsearch/filter-handler.ts
@@ -3,63 +3,24 @@
  * LICENSE.md file that was distributed with this source code.
  */
 import {API} from 'nouislider';
-import getQueryParameters from './urlparser';
-
 export default function (values: Array<string | number>, slider: API) {
   const {prestashop, Theme: {events}} = window;
 
-  // Prepare query parameters
-  // eslint-disable-next-line
-  let queryParams = <any> [];
+  const url = new URL(<string> slider.target.dataset.sliderEncodedUrl, window.location.origin);
+  let qParam = url.searchParams.get('q') || '';
 
-  // Get next encoded URL
-  const nextEncodedFacetsURL = <string> slider.target.dataset.sliderEncodedUrl;
-
-  // Split it to URL and parameters part
-  const urlsSplitted = nextEncodedFacetsURL.split('?');
-
-  // Retrieve parameters if exists
-  if (urlsSplitted !== undefined && urlsSplitted.length > 1) {
-    queryParams = getQueryParameters(urlsSplitted[1]);
-  }
-
-  // Check if q param is present, add it if missing
-  let found = false;
-  // eslint-disable-next-line
-  queryParams.forEach((query: any) => {
-    if (query.name === 'q') {
-      found = true;
-    }
-  });
-
-  if (!found) {
-    queryParams.push({name: 'q', value: ''});
-  }
-
-  // Update query parameter
-  // eslint-disable-next-line
-  queryParams.forEach((query: any) => {
-    if (query.name === 'q') {
-      // eslint-disable-next-line
-      query.value += [
-        query.value.length > 0 ? '/' : '',
-        slider.target.dataset.sliderLabel,
-        '-',
-        slider.target.dataset.sliderUnit,
-        '-',
-        values[0],
-        '-',
-        values[1],
-      ].join('');
-    }
-  });
-
-  const newUrl = [
-    urlsSplitted[0],
-    '?',
-    // eslint-disable-next-line
-    queryParams.map((query: any) => `${encodeURIComponent(query.name)}=${encodeURIComponent(query.value).replace(/%20/g, '+')}`).join('&'),
+  qParam += [
+    qParam.length > 0 ? '/' : '',
+    slider.target.dataset.sliderLabel,
+    '-',
+    slider.target.dataset.sliderUnit,
+    '-',
+    values[0],
+    '-',
+    values[1],
   ].join('');
 
-  prestashop.emit(events.updateFacets, newUrl);
+  url.searchParams.set('q', qParam);
+
+  prestashop.emit(events.updateFacets, url.href);
 }
